1d7b673d6c234e5424dc3f1c89647db90c08bf47
Green S <kaylasierrabartley@gmail.com>
2020-11-19 19:01:36 -0600

    added client-side storage

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function persistInput(input)
+{
+  var key = "input-" + input.id;
+
+  var storedValue = localStorage.getItem(key);
+
+  if (storedValue)
+      input.value = storedValue;
+
+  input.addEventListener('input', function ()
+  {
+      localStorage.setItem(key, input.value);
+  });
+}
+
+var inputElement = document.getElementById("zip");
+
+persistInput(inputElement);
